32ff84a7f1c8ac2a6d323b6ca876a00a33f4ac86
Chhaya Sengar <117908142+ChhayaSengar@users.noreply.github.com>
2023-04-28 15:21:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 //your JS code here. If required.
-window.onload = (event) => {
-	document.querySelector("body").
-	innerHtml = "DOM load success";
-}
+// window.onload = (event) => {
+// 	document.querySelector("body").
+// 	innerHtml = "DOM load success";
+// }
+ function domLoaded() {
+        const body = document.querySelector("body");
+        body.innerHTML = "DOM load success";
+      }
+
+      document.addEventListener("DOMContentLoaded", domLoaded);
